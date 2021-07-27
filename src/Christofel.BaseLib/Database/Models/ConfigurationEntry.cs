4fefc860d0ff5d946a456ea08cd1c54fced3bdf9
František Boháček <fandabohacek@gmail.com>
2021-07-27 18:03:05 +0200

    chore: add gitignore

---
+++ src/Christofel.BaseLib/Database/Models/ConfigurationEntry.cs
@@ -0,0 +1,9 @@
+namespace Christofel.BaseLib.Database.Models
+{
+    public class ConfigurationEntry
+    {
+        public string Name { get; set; } = null!;
+
+        public string Value { get; set; } = null!;
+    }
+}
