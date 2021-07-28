a4f32c043c92b8d36a0dbdd1904a44e5469573d7
František Boháček <fandabohacek@gmail.com>
2021-07-28 19:22:24 +0200

    fix(base): make convert config throw correct exception

--- src/Christofel.BaseLib/Configuration/Converters/ConvertConfigConverter.cs
+++ src/Christofel.BaseLib/Configuration/Converters/ConvertConfigConverter.cs
@@ -1,10 +1,25 @@
+using System;
+using Christofel.BaseLib.Exceptions;
+
 namespace Christofel.BaseLib.Configuration.Converters
 {
+    /// <summary>
+    /// Converter for Config made using Convert class to convert to basic types
+    /// like short, int, long, bool, byte etc.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class ConvertConfigConverter<T> : ConfigConverter<T>
     {
         public override T Convert(string value)
         {
-            return (T)System.Convert.ChangeType(value, typeof(T));
+            try
+            {
+                return (T) System.Convert.ChangeType(value, typeof(T));
+            }
+            catch (Exception e)
+            {
+                throw new ConverterException(value, GetType(), e);
+            }
         }
     }
 }
