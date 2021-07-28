8b7dbdd0c71d91ae5910dd300d5402eba68722f9
František Boháček <fandabohacek@gmail.com>
2021-07-28 19:24:46 +0200

    refactor: move plugin info into IHasPluginInfo

---
+++ src/Christofel.BaseLib/Plugins/IHasPluginInfo.cs
@@ -0,0 +1,12 @@
+namespace Christofel.BaseLib.Plugins
+{
+    /// <summary>
+    /// Abstraction of plugin info to be able to use info about plugin even after plugin was destroyed
+    /// </summary>
+    public interface IHasPluginInfo
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public string Version { get; }
+    }
+}
