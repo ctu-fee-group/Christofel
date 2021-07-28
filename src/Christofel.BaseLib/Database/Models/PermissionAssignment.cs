f981acb16dd95942fb9c87a94d468d886c9157a0
František Boháček <fandabohacek@gmail.com>
2021-07-28 19:27:49 +0200

    fix(base): add correct primary key to permission assignment

--- src/Christofel.BaseLib/Database/Models/PermissionAssignment.cs
+++ src/Christofel.BaseLib/Database/Models/PermissionAssignment.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Christofel.BaseLib.Database.Models
 {
     public class PermissionAssignment
     {
+        [Key]
         public int PermissionAssignmentId { get; set; }
 
         public string PermissionName { get; set; } = null!;
