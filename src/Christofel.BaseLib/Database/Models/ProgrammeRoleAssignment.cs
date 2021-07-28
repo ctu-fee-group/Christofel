1e6430126cdd06d87428abf97dceb98aacc7cbe7
František Boháček <fandabohacek@gmail.com>
2021-07-28 19:25:09 +0200

    fix(base): add correct primary key to programme role assignment

--- src/Christofel.BaseLib/Database/Models/ProgrammeRoleAssignment.cs
+++ src/Christofel.BaseLib/Database/Models/ProgrammeRoleAssignment.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Christofel.BaseLib.Database.Models
 {
     public class ProgrammeRoleAssignment
     {
+        [Key]
+        public int ProgrammeRoleAssignmentId { get; set; }
+        
         public string Programme { get; set; } = null!;
 
         public int AssignmentId { get; set; }
