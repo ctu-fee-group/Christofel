b95884cc23ac4e814746b24bb515020471f1ce95
František Boháček <fandabohacek@gmail.com>
2021-08-02 00:22:34 +0200

    feat(commands): add command handler creators
    Add creators for SlashCommandHandlers so handlers can be created dynamically from Delegates and execute methods with all command arguments correctly

---
+++ src/Christofel.CommandsLib/HandlerCreator/CommandHandlerCreatorUtils.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Christofel.CommandsLib.CommandsInfo;
+using Christofel.CommandsLib.Reflection;
+using Discord.WebSocket;
+
+namespace Christofel.CommandsLib.HandlerCreator
+{
+    public class CommandHandlerCreatorUtils
+    {
+        /// <summary>
+        /// Create SlashCommandHandler from given Delegate.
+        /// Uses <see cref="EfficientInvoker"/> for invoking the delegate faster.
+        /// </summary>
+        /// <param name="function"></param>
+        /// <param name="getArguments"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static SlashCommandHandler CreateHandler(Delegate function, Func<SocketSlashCommandData, IEnumerable<object>> getArguments)
+        {
+            EfficientInvoker invoker = EfficientInvoker.ForDelegate(function);
+            
+            return (command, token) =>
+            {
+                List<object?> args = new() {command};
+                args.AddRange(getArguments(command.Data));
+                args.Add(token);
+
+                object? data = invoker.Invoke(function, args.ToArray());
+
+                if (data is null)
+                {
+                    throw new InvalidOperationException($@"Command handler of {command.Data.Name} returned null instead of Task");
+                }
+
+                return (Task)data;
+            };
+        }
+    }
+}
---
+++ src/Christofel.CommandsLib/HandlerCreator/ICommandHandlerCreator.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Christofel.CommandsLib.CommandsInfo;
+using Discord;
+using Discord.WebSocket;
+
+namespace Christofel.CommandsLib.HandlerCreator
+{
+    public delegate Task CommandDelegate(SocketSlashCommand command, CancellationToken token);
+    public delegate Task CommandDelegate<in T1>(SocketSlashCommand command, T1 arg1, CancellationToken token);
+    public delegate Task CommandDelegate<in T1, in T2>(SocketSlashCommand command, T1 arg1, T2 arg2, CancellationToken token);
+    public delegate Task CommandDelegate<in T1, in T2, in T3>(SocketSlashCommand command, T1 arg1, T2 arg2, T3 arg3, CancellationToken token);
+    public delegate Task CommandDelegate<in T1, in T2, in T3, in T4>(SocketSlashCommand command, T1 arg1, T2 arg2, T3 arg3, T4 arg4, CancellationToken token);
+    
+    /// <summary>
+    /// Creator of SlashCommandHandler different types may be used for subcommands or custom matching
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface ICommandHandlerCreator<T>
+    {
+        /// <summary>
+        /// Creates SlashCommandHandler for given matches
+        /// </summary>
+        /// <param name="matchers">List of matchers that specify what function is matched given conditions</param>
+        /// <returns></returns>
+        public SlashCommandHandler CreateHandlerForCommand(IEnumerable<(Func<T, bool>, Delegate)> matchers);
+    }
+}
