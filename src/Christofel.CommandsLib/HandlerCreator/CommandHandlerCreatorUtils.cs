0a27d8263ceff8f86d3fca453ee2a51375b009dd
František Boháček <fandabohacek@gmail.com>
2021-08-03 00:32:08 +0200

    fix(commands): handlers did not handle optional arguments correctly
    Commands were sent without optional arguments if they weren't present.
    That caused a bug where options were wrongly matched to delegate arguments.
    Fixed this matching to parameter names instead of positions

--- src/Christofel.CommandsLib/HandlerCreator/CommandHandlerCreatorUtils.cs
+++ src/Christofel.CommandsLib/HandlerCreator/CommandHandlerCreatorUtils.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using Christofel.CommandsLib.CommandsInfo;
 using Christofel.CommandsLib.Reflection;
@@ -19,25 +21,76 @@ namespace Christofel.CommandsLib.HandlerCreator
         /// <param name="getArguments"></param>
         /// <returns></returns>
         /// <exception cref="InvalidOperationException"></exception>
-        public static SlashCommandHandler CreateHandler(Delegate function, Func<SocketSlashCommandData, IEnumerable<object?>?> getArguments)
+        public static SlashCommandHandler CreateHandler(Delegate function,
+            Func<SocketSlashCommandData, IEnumerable<object?>?> getArguments)
         {
             EfficientInvoker invoker = EfficientInvoker.ForDelegate(function);
-            
+
             return (command, token) =>
             {
                 List<object?> args = new() {command};
                 args.AddRange(getArguments(command.Data) ?? Enumerable.Empty<object?>());
                 args.Add(token);
 
-                object? data = invoker.Invoke(function, args.ToArray());
-
-                if (data is null)
-                {
-                    throw new InvalidOperationException($@"Command handler of {command.Data.Name} returned null instead of Task");
-                }
-
-                return (Task)data;
+                return Invoke(command.Data.Name, invoker, function, args.ToArray());
             };
+        }
+
+        /// <summary>
+        /// Create SlashCommandHandler from given Delegate.
+        /// Uses <see cref="EfficientInvoker"/> for invoking the delegate faster.
+        /// </summary>
+        /// <param name="function"></param>
+        /// <param name="getArguments"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static Func<SocketSlashCommand, T, CancellationToken, Task> CreateHandler<T>(Delegate function,
+            Func<SocketSlashCommandData, T, IEnumerable<object?>?> getArguments)
+        {
+            EfficientInvoker invoker = EfficientInvoker.ForDelegate(function);
+
+            return (command, helper, token) =>
+            {
+                List<object?> args = new() {command};
+                args.AddRange(getArguments(command.Data, helper) ?? Enumerable.Empty<object?>());
+                args.Add(token);
+
+                return Invoke(command.Data.Name, invoker, function, args.ToArray());
+            };
+        }
+
+        public static IEnumerable<object?> GetParametersFromOptions(Delegate function,
+            IEnumerable<SocketSlashCommandDataOption>? options)
+        {
+            ParameterInfo[] parameters = function.Method.GetParameters();
+            object?[] arguments = new object?[parameters.Length - 2];
+
+            if (options == null)
+            {
+                return arguments;
+            }
+
+            IEnumerable<SocketSlashCommandDataOption> socketSlashCommandDataOptions =
+                options as SocketSlashCommandDataOption[] ?? options.ToArray();
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                string name = parameters[i + 1].Name?.ToLower() ?? "";
+                arguments[i] = socketSlashCommandDataOptions.FirstOrDefault(x => x.Name == name)?.Value;
+            }
+
+            return arguments;
+        }
+
+        private static Task Invoke(string name, EfficientInvoker invoker, Delegate function, object?[] args)
+        {
+            object? data = invoker.Invoke(function, args.ToArray());
+
+            if (data is null)
+            {
+                throw new InvalidOperationException($@"Command handler of {name} returned null instead of Task");
+            }
+
+            return (Task) data;
         }
     }
 }
--- src/Christofel.CommandsLib/HandlerCreator/PlainCommandHandlerCreator.cs
+++ src/Christofel.CommandsLib/HandlerCreator/PlainCommandHandlerCreator.cs
@@ -11,18 +11,20 @@ namespace Christofel.CommandsLib.HandlerCreator
     /// Creates SlashCommandHandler for command without subcommands.
     /// It should receive only 1 matcher and that should always match
     /// </summary>
-    public class PlainCommandHandlerCreator : ICommandHandlerCreator<string>
+    public class PlainCommandHandlerCreator : ICommandHandlerCreator<string, Delegate>
     {
         public SlashCommandHandler CreateHandlerForCommand(IEnumerable<(Func<string, bool>, Delegate)> matchers)
         {
             var valueTuples = matchers as (Func<string, bool>, Delegate)[] ?? matchers.ToArray();
             if (valueTuples.Count() != 1)
             {
-                throw new InvalidOperationException("PlainCommandHandlerCreator can handle only one matcher that is always true");
+                throw new InvalidOperationException(
+                    "PlainCommandHandlerCreator can handle only one matcher that is always true");
             }
-            
+
             var matcher = valueTuples.FirstOrDefault();
-            return GetHandler(CommandHandlerCreatorUtils.CreateHandler(matcher.Item2, (data => data.Options.Select(x => x.Value))));
+            return GetHandler(CommandHandlerCreatorUtils.CreateHandler(matcher.Item2,
+                (data => CommandHandlerCreatorUtils.GetParametersFromOptions(matcher.Item2, data.Options))));
         }
 
         private SlashCommandHandler GetHandler(SlashCommandHandler handler)
