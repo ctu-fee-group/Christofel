f65b5d133e31a0fa67d5867e8f5b5d155f3fc06f
František Boháček <fandabohacek@gmail.com>
2021-08-03 18:07:29 +0200

    refactor(commands): use IUserMessage instead of RestUserMessage for verifying messages

--- src/Christofel.CommandsLib/Verifier/Interfaces/IHasRestUserMessage.cs
+++ src/Christofel.CommandsLib/Verifier/Interfaces/IHasRestUserMessage.cs
@@ -1,9 +1,10 @@
+using Discord;
 using Discord.Rest;
 
 namespace Christofel.CommandsLib.Verifier.Interfaces
 {
     public interface IHasRestUserMessage
     {
-        public RestUserMessage? Message { get; set; }
+        public IUserMessage? UserMessage { get; set; }
     }
 }
