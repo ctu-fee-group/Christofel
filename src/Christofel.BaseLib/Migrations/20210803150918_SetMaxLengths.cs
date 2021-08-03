b720d76f0a5e4abc31e223d69c62e9f510b168d5
František Boháček <fandabohacek@gmail.com>
2021-08-03 17:35:27 +0200

    chore: add migration to fix max lengths, new DuplicitUser field for DbUser

---
+++ src/Christofel.BaseLib/Migrations/20210803150918_SetMaxLengths.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Christofel.BaseLib.Migrations
+{
+    public partial class SetMaxLengths : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duplicity",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CtuUsername",
+                table: "Users",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DuplicitUserId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Programme",
+                table: "ProgrammeRoleAssignments",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PermissionName",
+                table: "Permissions",
+                type: "varchar(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_DuplicitUserId",
+                table: "Users",
+                column: "DuplicitUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Users_DuplicitUserId",
+                table: "Users",
+                column: "DuplicitUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Users_DuplicitUserId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_DuplicitUserId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DuplicitUserId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CtuUsername",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Duplicity",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Programme",
+                table: "ProgrammeRoleAssignments",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PermissionName",
+                table: "Permissions",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(512)",
+                oldMaxLength: 512)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
