f4d9325d7f33fa8eebe4418bfd26de0ecf2cf1ec
František Boháček <fandabohacek@gmail.com>
2021-08-07 16:53:48 +0200

    refactor(commands): make command registrator responsible for registration instead of slash command info
    Resolve #53

--- src/Christofel.CommandsLib/Commands/CommandsRegistrator.cs
+++ src/Christofel.CommandsLib/Commands/CommandsRegistrator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -6,9 +7,12 @@ using System.Threading.Tasks;
 using Christofel.BaseLib.Permissions;
 using Christofel.BaseLib.Plugins;
 using Christofel.CommandsLib.CommandsInfo;
+using Christofel.CommandsLib.Extensions;
 using Christofel.CommandsLib.Handlers;
+using Discord;
 using Discord.Rest;
 using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
 
 namespace Christofel.CommandsLib.Commands
 {
@@ -17,21 +21,25 @@ namespace Christofel.CommandsLib.Commands
     /// </summary>
     public class CommandsRegistrator : ICommandsRegistrator, IStartable, IRefreshable, IStoppable
     {
-        private readonly  ICommandsGroupProvider _commandGroups;
+        private readonly ICommandsGroupProvider _commandGroups;
         private readonly ICommandHolder _commandsHolder;
         private readonly IPermissionService _permissions;
         private readonly DiscordRestClient _client;
+        private CommandCache _cache;
 
-        public CommandsRegistrator(ICommandsGroupProvider commandGroups, ICommandHolder commandsHolder, IPermissionService permissions, DiscordRestClient client)
+        public CommandsRegistrator(ICommandsGroupProvider commandGroups, ICommandHolder commandsHolder,
+            IPermissionService permissions, DiscordRestClient client)
         {
             _commandGroups = commandGroups;
             _commandsHolder = commandsHolder;
             _permissions = permissions;
             _client = client;
+            _cache = new CommandCache(client);
         }
-        
+
         public async Task StartAsync(CancellationToken token = new CancellationToken())
         {
+            _cache.Reset();
             token.ThrowIfCancellationRequested();
             await Task.WhenAll(
                 _commandGroups.GetGroups().Select(x => x.SetupCommandsAsync(_commandsHolder, token)));
@@ -41,11 +49,13 @@ namespace Christofel.CommandsLib.Commands
 
         public Task StopAsync(CancellationToken token = new CancellationToken())
         {
+            _cache.Reset();
             return UnregisterCommandsAsync(_commandsHolder, token);
         }
 
         public Task RefreshAsync(CancellationToken token = new CancellationToken())
         {
+            _cache.Reset();
             return RefreshCommandsAndPermissionsAsync(_commandsHolder, token);
         }
 
@@ -55,8 +65,8 @@ namespace Christofel.CommandsLib.Commands
             {
                 try
                 {
-                    await heldCommand.Info.RegisterCommandAsync(_client, _permissions.Resolver, token);
-                    await heldCommand.Info.RegisterPermissionsAsync(_client, _permissions, token);
+                    await RegisterCommandAsync(heldCommand.Info, token);
+                    _permissions.RegisterPermission(heldCommand.Info.Permission);
                 }
                 catch (Exception e)
                 {
@@ -70,14 +80,175 @@ namespace Christofel.CommandsLib.Commands
         {
             return Task.WhenAll(
                 holder.Commands
-                    .Select(x => x.Info.UnregisterCommandAsync(_permissions, token)));
-            
+                    .Select(x => UnregisterCommandAsync(x.Info, token)));
         }
 
         public Task RefreshCommandsAndPermissionsAsync(ICommandHolder holder, CancellationToken token = default)
         {
             return Task.WhenAll(
-                holder.Commands.Select(x => x.Info.RefreshCommandAndPermissionsAsync(_permissions.Resolver, token)));
+                holder.Commands.Select(x => RefreshCommandAsync(x.Info, token)));
+        }
+
+        private async Task RefreshCommandAsync(SlashCommandInfo info, CancellationToken token = new CancellationToken())
+        {
+            if (info.Command == null)
+            {
+                throw new InvalidOperationException("Cannot refresh without the command registered");
+            }
+
+            await ModifyCommand(info, token);
+            await RefreshPermissions(info, token);
+        }
+
+        private async Task<IApplicationCommand> RegisterCommandAsync(SlashCommandInfo info,
+            CancellationToken token = new CancellationToken())
+        {
+            if (info.Command == null)
+            {
+                SlashCommandCreationProperties command = await SetDefaultPermissionAsync(info, token);
+
+                if (info.Global)
+                {
+                    info.Command = await CreateGlobalCommand(info, token);
+                }
+                else
+                {
+                    info.Command = await CreateGuildCommand(info, token);
+                }
+            }
+
+            info.Registered = true;
+            return info.Command;
+        }
+
+        private async Task UnregisterCommandAsync(SlashCommandInfo info,
+            CancellationToken token = new CancellationToken())
+        {
+            _permissions.UnregisterPermission(info.Permission);
+            if (info.Command != null)
+            {
+                await info.Command.DeleteAsync(new()
+                {
+                    CancelToken = token
+                });
+                info.Command = null;
+            }
+
+            info.Registered = false;
+        }
+
+        private async Task ModifyCommand(SlashCommandInfo info, CancellationToken token = new CancellationToken())
+        {
+            if (info.Command is RestGlobalCommand globalCommand &&
+                !info.Command.MatchesCreationProperties(info.BuiltCommand))
+            {
+                await globalCommand.ModifyAsync(props =>
+                    ModifyCommandProperties(info, props, token).GetAwaiter().GetResult());
+            }
+            else if (info.Command is RestGuildCommand guildCommand)
+            {
+                if (!info.Command.MatchesCreationProperties(info.BuiltCommand))
+                {
+                    await guildCommand.ModifyAsync(props =>
+                        ModifyCommandProperties(info, props, token).GetAwaiter().GetResult());
+                }
+
+                await RefreshPermissions(info, token);
+            }
+        }
+
+        private async Task RefreshPermissions(SlashCommandInfo info, CancellationToken token = new CancellationToken())
+        {
+            if (info.Command is RestGlobalCommand)
+            {
+                return; // Global commands cannot have permissions (at least not in Discord.NET yet)
+            }
+            else if (info.Command is RestGuildCommand guildCommand)
+            {
+                ApplicationCommandPermission[] permissions =
+                    await _permissions.Resolver.GetSlashCommandPermissionsAsync(info.Permission, token);
+                GuildApplicationCommandPermission? commandPermission = await guildCommand.GetCommandPermission();
+
+                if (commandPermission == null || !commandPermission.MatchesPermissions(permissions))
+                {
+                    await guildCommand.ModifyCommandPermissions(permissions);
+                }
+            }
+        }
+
+        private async Task<SlashCommandCreationProperties> SetDefaultPermissionAsync(
+            SlashCommandInfo info,
+            CancellationToken token = new CancellationToken())
+        {
+            info.BuiltCommand.DefaultPermission = await info.IsForEveryoneAsync(_permissions.Resolver, token);
+            return info.BuiltCommand;
+        }
+
+        private async Task<ApplicationCommandProperties> ModifyCommandProperties(
+            SlashCommandInfo info,
+            ApplicationCommandProperties command,
+            CancellationToken token = new CancellationToken())
+        {
+            command.Description = info.BuiltCommand.Description;
+            command.Name = info.BuiltCommand.Name;
+            command.Options = info.BuiltCommand.Options;
+            command.DefaultPermission = await info.IsForEveryoneAsync(_permissions.Resolver, token);
+            return command;
+        }
+
+        private async Task<RestApplicationCommand> CreateGlobalCommand(SlashCommandInfo info,
+            CancellationToken token = new CancellationToken())
+        {
+            RestGlobalCommand? globalCommand = await _cache.GetGlobalCommand(info.BuiltCommand.Name, token);
+
+            if (globalCommand != null)
+            {
+                info.Command = globalCommand;
+                await ModifyCommand(info, token);
+            }
+            else
+            {
+                globalCommand = await _client.CreateGlobalCommand(info.BuiltCommand, new RequestOptions()
+                {
+                    CancelToken = token
+                });
+                info.Command = globalCommand;
+
+                await RefreshPermissions(info, token);
+            }
+
+            return globalCommand;
+        }
+
+        private async Task<RestApplicationCommand> CreateGuildCommand(SlashCommandInfo info,
+            CancellationToken token = new CancellationToken())
+        {
+            if (info.GuildId == null)
+            {
+                throw new ArgumentException("GuildId cannot be null for guild commands");
+            }
+
+            RestGuildCommand? guildCommand =
+                await _cache.GetGuildCommand((ulong) info.GuildId, info.BuiltCommand.Name, token);
+
+            if (guildCommand != null)
+            {
+                info.Command = guildCommand;
+                await ModifyCommand(info, token);
+            }
+            else
+            {
+                guildCommand = await _client.CreateGuildCommand(info.BuiltCommand, (ulong) info.GuildId,
+                    new RequestOptions()
+                    {
+                        CancelToken = token
+                    });
+                info.Command = guildCommand;
+
+                await RefreshPermissions(info, token);
+            }
+
+            return guildCommand;
         }
     }
 }
--- src/Christofel.CommandsLib/CommandsInfo/SlashCommandInfo.cs
+++ src/Christofel.CommandsLib/CommandsInfo/SlashCommandInfo.cs
@@ -21,8 +21,6 @@ namespace Christofel.CommandsLib.CommandsInfo
     /// </summary>
     public class SlashCommandInfo
     {
-        private bool _modifyingExistingCommand;
-        
         public SlashCommandInfo(SlashCommandBuilder builder, string permission,
             SlashCommandHandler handler)
         {
@@ -59,7 +57,7 @@ namespace Christofel.CommandsLib.CommandsInfo
         /// <summary>
         /// Registered command that is set after calling RegisterCommandAsync
         /// </summary>
-        public RestApplicationCommand? Command { get; private set; }
+        public RestApplicationCommand? Command { get; set; }
         
         /// <summary>
         /// Permission of the command
@@ -87,191 +85,6 @@ namespace Christofel.CommandsLib.CommandsInfo
         public Task<bool> IsForEveryoneAsync(IPermissionsResolver resolver, CancellationToken token = new CancellationToken())
         {
             return resolver.HasPermissionAsync(Permission, DiscordTarget.Everyone, token);
-        }
-
-        /// <summary>
-        /// Refreshes command DefaultPermission and permissions
-        /// </summary>
-        /// <param name="resolver"></param>
-        /// <param name="token"></param>
-        /// <exception cref="InvalidOperationException"></exception>
-        public async Task RefreshCommandAndPermissionsAsync(IPermissionsResolver resolver, CancellationToken token = new CancellationToken())
-        {
-            if (BuiltCommand == null)
-            {
-                throw new InvalidOperationException("Cannot refresh without the command built");
-            }
-
-            if (Command == null)
-            {
-                throw new InvalidOperationException("Cannot refresh without the command registered");
-            }
-            
-            await ModifyCommand(resolver, token);
-            await RefreshPermissions(resolver, token);
-        }
-
-        /// <summary>
-        /// Registers commands to Discord
-        /// </summary>
-        /// <remarks>
-        /// If the commands are found on discord, they will be modified instead of registering them again.
-        /// For setting correct permissions <see cref="RegisterPermissionsAsync"/>
-        /// </remarks>
-        /// <param name="client"></param>
-        /// <param name="resolver"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        public async Task<IApplicationCommand> RegisterCommandAsync(DiscordRestClient client, IPermissionsResolver resolver, CancellationToken token = new CancellationToken())
-        {
-            if (Command == null)
-            {
-                SlashCommandCreationProperties command = await SetDefaultPermissionAsync(BuiltCommand, resolver, token);
-                
-                if (Global)
-                {
-                    Command = await CreateGlobalCommand(client, command, token);
-                }
-                else
-                {
-                    Command = await CreateGuildCommand(client, command, token);
-                }
-            }
-
-            Registered = true;
-            return Command;
-        }
-
-        /// <summary>
-        /// Unregisters the discord command
-        /// </summary>
-        /// <param name="permissions"></param>
-        /// <param name="token"></param>
-        public async Task UnregisterCommandAsync(IPermissionService permissions, CancellationToken token = new CancellationToken())
-        {
-            permissions.UnregisterPermission(Permission);
-
-            if (Command != null)
-            {
-                await Command.DeleteAsync(new ()
-                {
-                    CancelToken = token
-                });
-            }
-            
-            Registered = false;
-        }
-
-        /// <summary>
-        /// Registers correct permissions of the command
-        /// </summary>
-        /// <param name="client"></param>
-        /// <param name="permissions"></param>
-        /// <param name="token"></param>
-        public async Task RegisterPermissionsAsync(DiscordRestClient client, IPermissionService permissions, CancellationToken token = new CancellationToken())
-        {
-            IPermissionsResolver resolver = permissions.Resolver;
-            
-            permissions.RegisterPermission(Permission);
-            await RegisterCommandAsync(client, resolver, token);
-            await RefreshPermissions(resolver, token);
-        }
-        
-        private async Task ModifyCommand(IPermissionsResolver resolver, CancellationToken token = new CancellationToken())
-        {
-            if (Command is RestGlobalCommand globalCommand)
-            {
-                await globalCommand.ModifyAsync(props => ModifyDefaultPermissionAsync(props, resolver, token).GetAwaiter().GetResult());
-            }
-            else if (Command is RestGuildCommand guildCommand)
-            {
-                await guildCommand.ModifyCommandPermissions(await resolver.GetSlashCommandPermissionsAsync(Permission, token));
-            }
-        }
-
-        private async Task RefreshPermissions(IPermissionsResolver resolver, CancellationToken token = new CancellationToken())
-        {
-            if (Command is RestGlobalCommand)
-            {
-                return; // Global commands cannot have permissions (at least not in Discord.NET yet)
-            }
-            else if (Command is RestGuildCommand guildCommand)
-            {
-                ApplicationCommandPermission[] permissions = await resolver.GetSlashCommandPermissionsAsync(Permission, token);
-                GuildApplicationCommandPermission? commandPermission = await guildCommand.GetCommandPermission();
-
-                if (commandPermission == null || !commandPermission.MatchesPermissions(permissions))
-                {
-                    await guildCommand.ModifyCommandPermissions(permissions);
-                }
-            }
-        }
-        
-        private async Task<SlashCommandCreationProperties> SetDefaultPermissionAsync(SlashCommandCreationProperties command, IPermissionsResolver resolver, CancellationToken token = new CancellationToken())
-        {
-            command.DefaultPermission = await IsForEveryoneAsync(resolver, token);
-            return command;
-        }
-
-        private async Task<ApplicationCommandProperties> ModifyDefaultPermissionAsync(ApplicationCommandProperties command, IPermissionsResolver resolver, CancellationToken token = new CancellationToken())
-        {
-            command.DefaultPermission = await IsForEveryoneAsync(resolver, token);
-            return command;
-        }
-        
-        private async Task<RestApplicationCommand> CreateGlobalCommand(DiscordRestClient client, SlashCommandCreationProperties command, CancellationToken token = new CancellationToken())
-        {
-            RestApplication application = await client.GetApplicationInfoAsync();
-            RestGlobalCommand? globalCommand = (await client.GetGlobalApplicationCommands(new RequestOptions() {CancelToken = token}))
-                .FirstOrDefault(x => x.Name == command.Name && x.ApplicationId == application.Id);
-
-            _modifyingExistingCommand = globalCommand != null;
-
-            if (globalCommand != null && !globalCommand.MatchesCreationProperties(command))
-            {
-                await globalCommand.ModifyAsync(props =>
-                {
-                    props.Description = command.Description;
-                    props.Options = command.Options;
-                    props.DefaultPermission = command.DefaultPermission;
-                });
-            }
-            
-            globalCommand ??= await client.CreateGlobalCommand(command, new RequestOptions()
-            {
-                CancelToken = token
-            });
-
-            return globalCommand;
-        }
-        
-        private async Task<RestApplicationCommand> CreateGuildCommand(DiscordRestClient client, SlashCommandCreationProperties command, CancellationToken token = new CancellationToken())
-        {
-            if (GuildId == null)
-            {
-                throw new ArgumentException("GuildId cannot be null for guild commands");
-            }
-
-            RestApplication application = await client.GetApplicationInfoAsync();
-            RestGuildCommand? guildCommand = (await client.GetGuildApplicationCommands(GuildId.Value, new RequestOptions() {CancelToken = token}))
-                .FirstOrDefault(x => x.Name == command.Name && x.ApplicationId == application.Id);
-            
-            if (guildCommand != null && !guildCommand.MatchesCreationProperties(command))
-            {
-                await guildCommand.ModifyAsync(props =>
-                {
-                    props.Description = command.Description;
-                    props.Options = command.Options;
-                    props.DefaultPermission = command.DefaultPermission;
-                });
-            }
-            
-            guildCommand ??= await client.CreateGuildCommand(command, GuildId.Value, new RequestOptions()
-            {
-                CancelToken = token
-            });
-
-            return guildCommand;
         }
     }
 }
