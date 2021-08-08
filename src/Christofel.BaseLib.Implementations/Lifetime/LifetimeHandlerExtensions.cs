2af685b21c2a8a07123d0045d6e13aefaf8d53f2
František Boháček <fandabohacek@gmail.com>
2021-08-08 08:54:32 +0200

    feat(base): add common lifetime handler extensions for moving to state

---
+++ src/Christofel.BaseLib.Implementations/Lifetime/LifetimeHandlerExtensions.cs
@@ -0,0 +1,29 @@
+namespace Christofel.BaseLib.Lifetime
+{
+    public static class LifetimeHandlerExtensions
+    {
+        public static bool MoveToIfLower(this LifetimeHandler lifetimeHandler, LifetimeState state)
+        {
+            bool set = lifetimeHandler.Lifetime.State < state;
+
+            if (set)
+            {
+                lifetimeHandler.MoveToState(state);
+            }
+
+            return set;
+        }
+        
+        public static bool MoveToIfPrevious(this LifetimeHandler lifetimeHandler, LifetimeState state)
+        {
+            bool set = lifetimeHandler.Lifetime.State < state;
+
+            if (set)
+            {
+                lifetimeHandler.MoveToState(state);
+            }
+
+            return set;
+        }
+    }
+}
