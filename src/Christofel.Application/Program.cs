1d303abf8cf3c624472d648b3938b70f5f05c24e
František Boháček <fandabohacek@gmail.com>
2021-08-09 18:49:51 +0200

    fix(application): catch exception in Main

--- src/Christofel.Application/Program.cs
+++ src/Christofel.Application/Program.cs
@@ -8,13 +8,21 @@ namespace Christofel.Application
     {
         public static async Task Main(string[] args)
         {
-            ChristofelApp application = new ChristofelApp(args);
+            try
+            {
+                ChristofelApp application = new ChristofelApp(args);
 
-            // App lifetime cycle
-            await application.InitAsync();
-            await application.RunAsync(); // blocks until Bot.QuitBot is called
-            
-            Console.WriteLine("Goodbye!");
+                // App lifetime cycle
+                await application.InitAsync();
+                await application.RunAsync(); // blocks until Bot.QuitBot is called
+
+                Console.WriteLine("Goodbye!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Caught an exception in Main.");
+                Console.WriteLine(e);
+            }
         }
     }
 }
