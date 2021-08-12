e944989dbc710120de06883c2e9f588840addbf5
František Boháček <fandabohacek@gmail.com>
2021-08-12 17:01:35 +0200

    fix(usermap): logging of exceptions

--- src/Usermap/UsermapApiPeople.cs
+++ src/Usermap/UsermapApiPeople.cs
@@ -26,16 +26,19 @@ namespace Usermap
             {
                 return _cachedPeople[username];
             }
-            
+
             IRestRequest request = new RestRequest("/people/{username}", Method.GET)
                 .AddUrlSegment("username", username);
 
             IRestResponse<UsermapPerson?> response = await _client.ExecuteAsync<UsermapPerson?>(request, token);
             _cachedPeople[username] = response.Data;
-            
+
             if (!response.IsSuccessful || response.Data == null)
             {
-                _logger.LogWarning($"Could not obtain usermap user information({username}): {response.StatusCode} {response.ErrorMessage} {response.Content}");
+                _logger.LogWarning(
+                    response.ErrorException,
+                    $"Could not obtain usermap user information({username}): {response.StatusCode} {response.ErrorMessage} {response.Content}"
+                );
                 return null;
             }
 
