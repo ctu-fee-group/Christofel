41248709292ea37616eca95438dcfa757c270adc
František Boháček <fandabohacek@gmail.com>
2021-08-12 17:02:37 +0200

    feat(base): add migration for registration token and specific role assignments

---
+++ src/Christofel.BaseLib/Migrations/20210812085458_AddRegistrationTokenSpecificRoleAssignments.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Christofel.BaseLib.Migrations
+{
+    public partial class AddRegistrationTokenSpecificRoleAssignments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CtuUsername",
+                table: "Users",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationCode",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "SpecificRoleAssignments",
+                columns: table => new
+                {
+                    SpecificRoleAssignmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AssignmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpecificRoleAssignments", x => x.SpecificRoleAssignmentId);
+                    table.ForeignKey(
+                        name: "FK_SpecificRoleAssignments_RoleAssignments_AssignmentId",
+                        column: x => x.AssignmentId,
+                        principalTable: "RoleAssignments",
+                        principalColumn: "RoleAssignmentId",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpecificRoleAssignments_AssignmentId",
+                table: "SpecificRoleAssignments",
+                column: "AssignmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SpecificRoleAssignments");
+
+            migrationBuilder.DropColumn(
+                name: "RegistrationCode",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CtuUsername",
+                table: "Users",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
