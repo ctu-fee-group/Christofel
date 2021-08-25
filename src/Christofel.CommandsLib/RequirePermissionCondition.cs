832e4005c1fe384e2453d0fb44ca00fb04966d0d
František Boháček <fandabohacek@gmail.com>
2021-08-25 22:07:06 +0200

    feat(commands): allow usage of commands using messages and check permissions accordingly

--- src/Christofel.CommandsLib/RequirePermissionCondition.cs
+++ src/Christofel.CommandsLib/RequirePermissionCondition.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Remora.Commands.Conditions;
+using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.API.Objects;
 using Remora.Discord.Commands.Contexts;
+using Remora.Discord.Core;
 using Remora.Results;
 
 namespace Christofel.CommandsLib
@@ -27,32 +30,56 @@ namespace Christofel.CommandsLib
         public async ValueTask<Result> CheckAsync(RequirePermissionAttribute attribute,
             CancellationToken ct = new CancellationToken())
         {
-            if (_context is not InteractionContext interactionContext)
-            {
-                return new InvalidOperationException("Cannot execute the command outside of itneractions");
-            }
-
             bool result = false;
-            if (interactionContext.Member.HasValue)
+            var roles = GetRoles();
+
+            if (roles.HasValue)
             {
-                result = await _permissionResolver.HasPermissionAsync(interactionContext.Member.Value,
+                result = await _permissionResolver.HasPermissionAsync(_context.User.ID, roles.Value,
                     attribute.Permission, ct);
             }
             else
             {
-                result = await _permissionResolver.HasPermissionAsync(interactionContext.User, attribute.Permission,
+                result = await _permissionResolver.HasPermissionAsync(_context.User, attribute.Permission,
                     ct);
             }
 
             if (!result)
             {
                 _logger.LogWarning(
-                    $"User <@{interactionContext.User.ID.Value}> ({interactionContext.User.Username}#{interactionContext.User.Discriminator}) tried to execute command {interactionContext.Data.Name}, but does not have sufficient permissions");
+                    $"User <@{_context.User.ID.Value}> ({_context.User.Username}#{_context.User.Discriminator}) tried to execute command {GetCommandName()}, but does not have sufficient permissions");
             }
 
             return result
                 ? Result.FromSuccess()
                 : new InvalidOperationException("You don't have sufficient permissions");
+        }
+
+        private string GetCommandName()
+        {
+            if (_context is InteractionContext interactionContext && interactionContext.Data.Name.HasValue)
+            {
+                return interactionContext.Data.Name.Value;
+            }
+
+            return "(Unknown name)";
+        }
+
+        private Optional<IReadOnlyList<Snowflake>> GetRoles()
+        {
+            if (_context is InteractionContext interactionContext)
+            {
+                return interactionContext.Member.HasValue
+                    ? new Optional<IReadOnlyList<Snowflake>>(interactionContext.Member.Value.Roles)
+                    : default;
+            }
+            else if (_context is MessageContext messageContext)
+            {
+                var partialMember = messageContext.Message.Member;
+                return partialMember.HasValue ? partialMember.Value.Roles : default;
+            }
+
+            return default;
         }
     }
 }
