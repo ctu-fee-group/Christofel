4cc5e0fc5843a26cff628bc56d566c81f4d1bd87
František Boháček <fandabohacek@gmail.com>
2021-08-26 14:06:17 +0200

    fix(commands): correctly send validation result to the user

--- src/Christofel.CommandsLib/Validator/ValidationErrorHandler.cs
+++ src/Christofel.CommandsLib/Validator/ValidationErrorHandler.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation.Results;
 using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.API.Objects;
 using Remora.Discord.Commands.Contexts;
+using Remora.Discord.Commands.Feedback.Services;
 using Remora.Discord.Commands.Services;
 using Remora.Results;
 
@@ -11,11 +19,16 @@ namespace Christofel.CommandsLib.Validator
     {
         private readonly ILogger _logger;
         private readonly ICommandContext _context;
-        private readonly ValidationFeedbackService _feedbackService;
+        private readonly FeedbackService _feedbackService;
+        private readonly IDiscordRestChannelAPI _channelApi;
+        private readonly IDiscordRestInteractionAPI _interactionApi;
 
-        public ValidationErrorHandler(ICommandContext context, ValidationFeedbackService validationFeedbackService,
-            ILogger<ValidationErrorHandler> logger)
+        public ValidationErrorHandler(ICommandContext context, FeedbackService validationFeedbackService,
+            ILogger<ValidationErrorHandler> logger, IDiscordRestChannelAPI channelApi,
+            IDiscordRestInteractionAPI interactionApi)
         {
+            _interactionApi = interactionApi;
+            _channelApi = channelApi;
             _logger = logger;
             _context = context;
             _feedbackService = validationFeedbackService;
@@ -26,8 +39,10 @@ namespace Christofel.CommandsLib.Validator
         {
             if (!commandResult.IsSuccess && commandResult.Error is ValidationResultError validationResultError)
             {
-                _logger.LogWarning($"User <@{_context.User}> ({_context.User.Username}#{_context.User.Discriminator}) has put in invalid data to command, see errors:\n{validationResultError.Message}");
-                var feedbackResult = await _feedbackService.SendContextualValidationError(validationResultError.ValidationFailures, ct);
+                _logger.LogWarning(
+                    $"User <@{_context.User}> ({_context.User.Username}#{_context.User.Discriminator}) has put in invalid data to command, see errors:\n{validationResultError.Message}");
+                var feedbackResult = await SendValidationEmbed(context,
+                    GetValidationEmbed(validationResultError.ValidationFailures), ct);
 
                 return feedbackResult.IsSuccess
                     ? Result.FromSuccess()
@@ -35,6 +50,59 @@ namespace Christofel.CommandsLib.Validator
             }
 
             return Result.FromSuccess();
+        }
+
+        private Embed GetValidationEmbed(IReadOnlyList<ValidationFailure> validationFailures)
+        {
+            var embed = new Embed(
+                "Validation errors",
+                EmbedType.Rich,
+                Fields: validationFailures.Select(GetField).ToList(),
+                Colour: _feedbackService.Theme.FaultOrDanger
+            );
+
+            return embed;
+        }
+
+        private IEmbedField GetField(ValidationFailure validationFailure)
+        {
+            return new EmbedField(validationFailure.PropertyName, validationFailure.ErrorMessage);
+        }
+
+        private async Task<Result<IMessage>> SendValidationEmbed(ICommandContext context, Embed embed,
+            CancellationToken ct)
+        {
+            switch (context)
+            {
+                case MessageContext messageContext:
+                {
+                    return await _channelApi.CreateMessageAsync
+                    (
+                        messageContext.ChannelID,
+                        embeds: new[] { embed },
+                        ct: ct
+                    );
+                }
+                case InteractionContext interactionContext:
+                {
+                    var result = await _interactionApi.CreateFollowupMessageAsync
+                    (
+                        interactionContext.ApplicationID,
+                        interactionContext.Token,
+                        embeds: new[] { embed },
+                        ct: ct
+                    );
+
+                    if (!result.IsSuccess)
+                    {
+                        return result;
+                    }
+
+                    return result;
+                }
+                default:
+                    throw new InvalidOperationException("Invalid context type");
+            }
         }
     }
 }
