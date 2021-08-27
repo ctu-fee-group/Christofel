70c5d7f8c7af1e4a910907bb8b8ead2aa7f4ebae
František Boháček <fandabohacek@gmail.com>
2021-08-27 20:07:17 +0200

    chore(api): add initial migration for api cache database

---
+++ src/Christofel.Api/Migrations/20210827180519_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Christofel.Api.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AssignRoles",
+                columns: table => new
+                {
+                    AssignRoleId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserDiscordId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    GuildDiscordId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    RoleId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    Add = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignRoles", x => x.AssignRoleId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssignRoles");
+        }
+    }
+}
