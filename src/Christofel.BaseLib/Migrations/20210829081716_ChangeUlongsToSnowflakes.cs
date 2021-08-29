dc13a105d3b68dabd92946e571b84a8d5253b00d
František Boháček <fandabohacek@gmail.com>
2021-08-29 10:18:37 +0200

    feat(base): add migration to convert ulong values to long

---
+++ src/Christofel.BaseLib/Migrations/20210829081716_ChangeUlongsToSnowflakes.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Christofel.BaseLib.Migrations
+{
+    public partial class ChangeUlongsToSnowflakes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "DiscordId",
+                table: "Users",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RoleId",
+                table: "RoleAssignments",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Target_DiscordId",
+                table: "Permissions",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<ulong>(
+                name: "DiscordId",
+                table: "Users",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<ulong>(
+                name: "RoleId",
+                table: "RoleAssignments",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<ulong>(
+                name: "Target_DiscordId",
+                table: "Permissions",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
