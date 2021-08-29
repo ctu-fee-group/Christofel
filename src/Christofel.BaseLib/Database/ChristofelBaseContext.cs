f870e9b2ddeb0b153b19d0692060837452d8eeba
František Boháček <fandabohacek@gmail.com>
2021-08-29 10:18:13 +0200

    fix(base): correctly register conversion for owned DiscordType

--- src/Christofel.BaseLib/Database/ChristofelBaseContext.cs
+++ src/Christofel.BaseLib/Database/ChristofelBaseContext.cs
@@ -23,15 +23,17 @@ namespace Christofel.BaseLib.Database
             modelBuilder.Entity<DbUser>()
                 .Property(x => x.DiscordId)
                 .HasConversion(v => (long)v.Value, v => new Snowflake((ulong)v));
-            
-            modelBuilder.Entity<DiscordTarget>()
-                .Property(x => x.DiscordId)
-                .HasConversion(v => (long)v.Value, v => new Snowflake((ulong)v));
-            
+
             modelBuilder.Entity<RoleAssignment>()
                 .Property(x => x.RoleId)
                 .HasConversion(v => (long)v.Value, v => new Snowflake((ulong)v));
-            
+
+            modelBuilder.Entity<PermissionAssignment>()
+                .OwnsOne(x => x.Target,
+                    b => b
+                        .Property(x => x.DiscordId)
+                        .HasConversion(v => (long)v.Value, v => new Snowflake((ulong)v)));
+
             modelBuilder.Entity<DbUser>()
                 .HasOne<DbUser>(x => x.DuplicitUser!)
                 .WithMany(x => x.DuplicitUsersBack!)
@@ -43,25 +45,25 @@ namespace Christofel.BaseLib.Database
                 .WithMany(x => x.ProgrammeRoleAssignments)
                 .HasForeignKey(x => x.AssignmentId)
                 .OnDelete(DeleteBehavior.Restrict);
-            
+
             modelBuilder.Entity<TitleRoleAssignment>()
                 .HasOne<RoleAssignment>(x => x.Assignment)
                 .WithMany(x => x.TitleRoleAssignments)
                 .HasForeignKey(x => x.AssignmentId)
                 .OnDelete(DeleteBehavior.Restrict);
-            
+
             modelBuilder.Entity<UsermapRoleAssignment>()
                 .HasOne<RoleAssignment>(x => x.Assignment)
                 .WithMany(x => x.UsermapRoleAssignments)
                 .HasForeignKey(x => x.AssignmentId)
                 .OnDelete(DeleteBehavior.Restrict);
-            
+
             modelBuilder.Entity<YearRoleAssignment>()
                 .HasOne<RoleAssignment>(x => x.Assignment)
                 .WithMany(x => x.YearRoleAssignments)
                 .HasForeignKey(x => x.AssignmentId)
                 .OnDelete(DeleteBehavior.Restrict);
-            
+
             modelBuilder.Entity<SpecificRoleAssignment>()
                 .HasOne<RoleAssignment>(x => x.Assignment)
                 .WithMany(x => x.SpecificRoleAssignments)
@@ -69,7 +71,8 @@ namespace Christofel.BaseLib.Database
                 .OnDelete(DeleteBehavior.Restrict);
         }
 
-        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+            CancellationToken cancellationToken = new CancellationToken())
         {
             this.AddTimestamps();
             return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
@@ -84,13 +87,13 @@ namespace Christofel.BaseLib.Database
         public DbSet<DbUser> Users => Set<DbUser>();
         public DbSet<PermissionAssignment> Permissions => Set<PermissionAssignment>();
         public DbSet<RoleAssignment> RoleAssignments => Set<RoleAssignment>();
-        
+
         public DbSet<YearRoleAssignment> YearRoleAssignments => Set<YearRoleAssignment>();
         public DbSet<SpecificRoleAssignment> SpecificRoleAssignments => Set<SpecificRoleAssignment>();
         public DbSet<ProgrammeRoleAssignment> ProgrammeRoleAssignments => Set<ProgrammeRoleAssignment>();
         public DbSet<UsermapRoleAssignment> UsermapRoleAssignments => Set<UsermapRoleAssignment>();
         public DbSet<TitleRoleAssignment> TitleRoleAssignment => Set<TitleRoleAssignment>();
-        
+
         IQueryable<TEntity> IReadableDbContext.Set<TEntity>() where TEntity : class
         {
             return Set<TEntity>().AsNoTracking();
