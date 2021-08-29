e72b6a9382aabd2bae7dcaa9fd55e226da914d67
František Boháček <fandabohacek@gmail.com>
2021-08-29 10:32:15 +0200

    feat(api): add migration to convert ulong values to long

---
+++ src/Christofel.Api/Migrations/20210829081908_ChangeUlongsToSnowflakes.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Christofel.Api.Migrations
+{
+    public partial class ChangeUlongsToSnowflakes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "UserDiscordId",
+                table: "AssignRoles",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RoleId",
+                table: "AssignRoles",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "GuildDiscordId",
+                table: "AssignRoles",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<ulong>(
+                name: "UserDiscordId",
+                table: "AssignRoles",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<ulong>(
+                name: "RoleId",
+                table: "AssignRoles",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<ulong>(
+                name: "GuildDiscordId",
+                table: "AssignRoles",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
