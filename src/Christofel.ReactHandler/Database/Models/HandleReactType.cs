d9d7112197bba1c59869f335423bb35e34082f91
František Boháček <fandabohacek@gmail.com>
2021-09-06 20:48:40 +0200

    feat(reacthandler): add database context and models

---
+++ src/Christofel.ReactHandler/Database/Models/HandleReactType.cs
@@ -0,0 +1,11 @@
+namespace Christofel.ReactHandler.Database.Models
+{
+    /// <summary>
+    /// Type of entity that should be added/removed
+    /// </summary>
+    public enum HandleReactType
+    {
+        Role,
+        Channel
+    }
+}
