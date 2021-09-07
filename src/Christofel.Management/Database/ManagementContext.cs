8f76a09981c0f453b460adfa5d6f11748f0c8966
František Boháček <fandabohacek@gmail.com>
2021-09-07 22:57:41 +0200

    fix(management): add conversion for snowflake database properties

--- src/Christofel.Management/Database/ManagementContext.cs
+++ src/Christofel.Management/Database/ManagementContext.cs
@@ -2,12 +2,24 @@ using System.Linq;
 using Christofel.BaseLib.Database;
 using Christofel.Management.Database.Models;
 using Microsoft.EntityFrameworkCore;
+using Remora.Discord.Core;
 
 namespace Christofel.Management.Database
 {
     public class ManagementContext : DbContext, IReadableDbContext<ManagementContext>
     {
         public DbSet<TemporalSlowmode> TemporalSlowmodes => Set<TemporalSlowmode>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TemporalSlowmode>()
+                .Property(x => x.ChannelId)
+                .HasConversion(v => (long)v.Value, v => new Snowflake((ulong)v));
+            
+            modelBuilder.Entity<TemporalSlowmode>()
+                .Property(x => x.UserId)
+                .HasConversion(v => (long)v.Value, v => new Snowflake((ulong)v));
+        }
 
         IQueryable<TEntity> IReadableDbContext.Set<TEntity>() where TEntity : class
         {
