9672db56ce19ae70f81dffb2a4b1e90c6b9bcfc9
František Boháček <fandabohacek@gmail.com>
2021-09-07 23:05:32 +0200

    feat(management): add migrations for management context

---
+++ src/Christofel.Management/Migrations/20210907210440_InitialMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Christofel.Management.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TemporalSlowmodes",
+                columns: table => new
+                {
+                    TemporalSlowmodeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    ChannelId = table.Column<long>(type: "bigint", nullable: false),
+                    Interval = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    DeactivationDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ActivationDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemporalSlowmodes", x => x.TemporalSlowmodeId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TemporalSlowmodes");
+        }
+    }
+}
