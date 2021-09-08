0a34f8df48ac67ea5bbd95a8eb4b294a6e80d5f3
František Boháček <fandabohacek@gmail.com>
2021-09-08 22:02:41 +0200

    feat(application): make plugin autoloader detach and reload plugins

--- src/Core/Christofel.Application/Plugins/PluginAutoloader.cs
+++ src/Core/Christofel.Application/Plugins/PluginAutoloader.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Christofel.BaseLib;
 using Christofel.BaseLib.Plugins;
+using Christofel.Plugins;
+using Christofel.Plugins.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -12,42 +15,53 @@ namespace Christofel.Application.Plugins
     /// Auto loads plugins specified in configuration
     /// on startup
     /// </summary>
-    public class PluginAutoloader : IStartable
+    public class PluginAutoloader : IStartable, IRefreshable, IStoppable
     {
-        private readonly PluginServiceOptions _options;
+        private readonly PluginAutoloaderOptions _options;
+        private readonly PluginStorage _pluginStorage;
         private readonly PluginService _plugins;
-        private readonly IChristofelState _state;
         private readonly ILogger<PluginAutoloader> _logger;
-        
-        public PluginAutoloader(IOptions<PluginServiceOptions> options,
+
+        public PluginAutoloader(IOptions<PluginAutoloaderOptions> options,
             ILogger<PluginAutoloader> logger,
             PluginService plugins,
-            IChristofelState state)
+            PluginStorage pluginStorage)
         {
-            _state = state;
+            _pluginStorage = pluginStorage;
             _plugins = plugins;
             _options = options.Value;
             _logger = logger;
         }
-        
+
         public async Task StartAsync(CancellationToken token = new CancellationToken())
         {
             if (_options.AutoLoad == null)
             {
                 return;
             }
-            
+
             foreach (string module in _options.AutoLoad)
             {
                 try
                 {
-                    await _plugins.AttachAsync(_state, module, token);
+                    await _plugins.AttachAsync(module, token);
                 }
                 catch (Exception e)
                 {
                     _logger.LogError(e, $@"Could not attach autoload module {module}");
                 }
             }
+        }
+
+        public Task RefreshAsync(CancellationToken token = new CancellationToken())
+        {
+            return Task.WhenAll(_pluginStorage.AttachedPlugins.Select(x => x.Plugin).OfType<IChristofelRuntimePlugin>()
+                .Select(x => x.RefreshAsync(token)));
+        }
+
+        public Task StopAsync(CancellationToken token = new CancellationToken())
+        {
+            return _plugins.DetachAllAsync(token);
         }
     }
 }
