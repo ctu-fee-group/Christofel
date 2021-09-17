b282e18ad3900d12687ded261c557ea3afb61fd2
František Boháček <fandabohacek@gmail.com>
2021-09-17 18:47:12 +0200

    fix(helpers): include scheduling into plugin context

--- src/Libs/Christofel.Helpers/Plugins/PluginContext.cs
+++ src/Libs/Christofel.Helpers/Plugins/PluginContext.cs
@@ -5,6 +5,7 @@
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Christofel.Remora;
+using Christofel.Scheduling;
 
 namespace Christofel.BaseLib.Plugins
 {
