a2204cf2f9bcf52b45223c14688cbd8773c752ef
František Boháček <fandabohacek@gmail.com>
2021-11-28 20:17:32 +0100

    chore: add production docker setup

---
+++ docker/production/migrator/migrate.sh
@@ -0,0 +1,7 @@
+export PATH="$PATH:/root/.dotnet/tools"
+cp ./config.json ./Tools/Christofel.Design/bin/Debug/net6.0
+
+dotnet ef database --startup-project=./Tools/Christofel.Design/ update --context ChristofelBaseContext -p ./Core/Christofel.Common
+dotnet ef database --startup-project=./Tools/Christofel.Design/ update --context ManagementContext -p ./Plugins/Christofel.Management
+dotnet ef database --startup-project=./Tools/Christofel.Design/ update --context ReactHandlerContext -p ./Plugins/Christofel.ReactHandler
+dotnet ef database --startup-project=./Tools/Christofel.Design/ update --context ApiCacheContext -p ./Plugins/Christofel.Api
