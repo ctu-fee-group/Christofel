776188d1de79788a45ebde8986ca797403a856a3
František Boháček <fandabohacek@gmail.com>
2022-08-25 17:28:11 +0200

    feat(api): make sure the latest student role is used as a programme

--- src/Plugins/Christofel.Api/Ctu/Auth/Steps/ProgrammeRoleStep.cs
+++ src/Plugins/Christofel.Api/Ctu/Auth/Steps/ProgrammeRoleStep.cs
@@ -4,12 +4,15 @@
 //   Copyright (c) Christofel authors. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Kos;
 using Kos.Abstractions;
+using Kos.Atom;
+using Kos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Remora.Results;
@@ -34,7 +37,8 @@ namespace Christofel.Api.Ctu.Auth.Steps
         /// <param name="logger">The logger.</param>
         /// <param name="kosPeopleApi">The kos people api.</param>
         /// <param name="kosStudentsApi">The kos students api.</param>
-        public ProgrammeRoleStep(ILogger<ProgrammeRoleStep> logger, IKosPeopleApi kosPeopleApi, IKosStudentsApi kosStudentsApi)
+        public ProgrammeRoleStep
+            (ILogger<ProgrammeRoleStep> logger, IKosPeopleApi kosPeopleApi, IKosStudentsApi kosStudentsApi)
         {
             _kosPeopleApi = kosPeopleApi;
             _kosStudentsApi = kosStudentsApi;
@@ -47,40 +51,67 @@ namespace Christofel.Api.Ctu.Auth.Steps
             var kosPerson =
                 await _kosPeopleApi.GetPersonAsync(data.LoadedUser.CtuUsername, ct);
 
-            var studentLoadable = kosPerson?.Roles.Students.LastOrDefault();
-            if (studentLoadable is not null)
+            var student = await GetLastStudentRole(kosPerson?.Roles.Students, ct);
+
+            if (student is null)
             {
-                var student = await _kosStudentsApi.GetStudent(studentLoadable, token: ct);
-
-                if (student is null)
-                {
-                    return Result.FromSuccess();
-                }
-
-                var programmeTitle = student.Programme?.Title;
-
-                if (programmeTitle is null)
-                {
-                    return Result.FromSuccess();
-                }
-
-                List<CtuAuthRole> roles = await data.DbContext.ProgrammeRoleAssignments
-                    .AsNoTracking()
-                    .Where(x => x.Programme == programmeTitle)
-                    .Include(x => x.Assignment)
-                    .Select(x => new CtuAuthRole { RoleId = x.Assignment.RoleId, Type = x.Assignment.RoleType })
-                    .ToListAsync(ct);
-
-                if (roles.Count == 0)
-                {
-                    _logger.LogWarning
-                        ($"Could not find mapping for programme {programmeTitle} for user {data.GuildUser}");
-                }
-
-                data.Roles.AddRange(roles);
+                return Result.FromSuccess();
             }
 
+            var programmeTitle = student.Programme?.Title;
+
+            if (programmeTitle is null)
+            {
+                return Result.FromSuccess();
+            }
+
+            List<CtuAuthRole> roles = await data.DbContext.ProgrammeRoleAssignments
+                .AsNoTracking()
+                .Where(x => x.Programme == programmeTitle)
+                .Include(x => x.Assignment)
+                .Select(x => new CtuAuthRole { RoleId = x.Assignment.RoleId, Type = x.Assignment.RoleType })
+                .ToListAsync(ct);
+
+            if (roles.Count == 0)
+            {
+                _logger.LogWarning
+                    ($"Could not find mapping for programme {programmeTitle} for user {data.GuildUser}");
+            }
+
+            data.Roles.AddRange(roles);
+
             return Result.FromSuccess();
+        }
+
+        private async Task<Student?> GetLastStudentRole
+            (List<AtomLoadableEntity<Student>>? studentRoles, CancellationToken ct)
+        {
+            Student? latestStudent = null;
+            DateTime? latestDate = null;
+
+            if (studentRoles is null)
+            {
+                return null;
+            }
+
+            foreach (var studentRole in studentRoles)
+            {
+                var currentStudent = await _kosStudentsApi.GetStudent(studentRole, token: ct);
+
+                if (currentStudent is null)
+                {
+                    continue;
+                }
+
+                var currentStudentStartDate = currentStudent.StartDate ?? DateTime.Today;
+                if (latestDate is null || currentStudentStartDate > latestDate)
+                {
+                    latestStudent = currentStudent;
+                    latestDate = currentStudentStartDate;
+                }
+            }
+
+            return latestStudent;
         }
     }
 }
