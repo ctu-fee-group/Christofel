17309074fb8cb587446b9b389194b320bccd60f3
František Boháček <fandabohacek@gmail.com>
2023-02-16 21:21:03 +0100

    chore: remove contextual parsers as Remora parses partial types now as well

--- src/Libs/Christofel.CommandsLib/Extensions/ServiceCollectionExtensions.cs
+++ src/Libs/Christofel.CommandsLib/Extensions/ServiceCollectionExtensions.cs
@@ -4,7 +4,6 @@
 //   Copyright (c) Christofel authors. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Christofel.CommandsLib.ContextedParsers;
 using Christofel.CommandsLib.ExecutionEvents;
 using Christofel.CommandsLib.Permissions;
 using Christofel.CommandsLib.Validator;
@@ -35,10 +34,6 @@ namespace Christofel.CommandsLib.Extensions
         /// <see cref="IRefreshable"/>, <see cref="IStoppable"/> service for registering
         /// the commands using <see cref="ChristofelSlashService"/>.
         ///
-        /// Adds contextual parsers for less resource heavy parsing
-        /// <see cref="ContextualChannelParser"/>, <see cref="ContextualRoleParser"/>,
-        /// <see cref="ContextualUserParser"/>, <see cref="ContextualGuildMemberParser"/>.
-        ///
         /// Adds <see cref="ValidationFeedbackService"/> for generating embeds with wrong validation data.
         /// </remarks>
         /// <param name="collection">The collection to add commands into.</param>
@@ -49,13 +44,6 @@ namespace Christofel.CommandsLib.Extensions
             .AddScoped<ValidationFeedbackService>()
             .AddTransient<ChristofelCommandPermissionResolver>()
             .AddStateful<ChristofelCommandRegistrator>(ServiceLifetime.Transient)
-
-            // parsers
-            .AddParser<ContextualUserParser>()
-            .AddParser<ContextualGuildMemberParser>()
-            .AddParser<ContextualRoleParser>()
-            .AddParser<ContextualChannelParser>()
-            .AddParser<SnowflakeParser>()
 
             // conditions
             .AddCondition<RequirePermissionCondition>()
