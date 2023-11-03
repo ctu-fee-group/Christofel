4f86c299a517acb230ff5970957e6a424d94d79e
Rutherther <rutherther@proton.me>
2023-11-03 23:50:05 +0100

    chore: add migration for optional programme role assignment

---
+++ src/Core/Christofel.Common/Migrations/20231103224922_ProgrammeRole_MakeRoleAssignmentOptional.cs
@@ -0,0 +1,44 @@
+﻿//
+//  20231103224922_ProgrammeRole_MakeRoleAssignmentOptional.cs
+//
+//  Copyright (c) Christofel authors. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Christofel.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProgrammeRole_MakeRoleAssignmentOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignmentId",
+                schema: "Core",
+                table: "ProgrammeRoleAssignment",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignmentId",
+                schema: "Core",
+                table: "ProgrammeRoleAssignment",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
