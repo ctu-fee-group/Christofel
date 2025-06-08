b74b52a91e5aff62e46fb236f115036ffc20b0d2
Rutherther <rutherther@ditigal.xyz>
2025-06-08 20:19:37 +0200

    feat(migrations): add migration for username role assignment

--- src/Core/Christofel.Common/Migrations/20250608181803_AddUsernameRoleAssignment.Designer.cs
+++ src/Core/Christofel.Common/Migrations/20250608181803_AddUsernameRoleAssignment.Designer.cs
@@ -4,6 +4,7 @@ using Christofel.Common.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Christofel.Common.Migrations
 {
     [DbContext(typeof(ChristofelBaseContext))]
-    partial class ChristofelBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250608181803_AddUsernameRoleAssignment")]
+    partial class AddUsernameRoleAssignment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
