77d461016d272f738d9d4a1706d19680d3e6e19c
Rutherther <rutherther@ditigal.xyz>
2025-08-23 22:41:01 +0200

    tests(ctuauth): do not construct GuildMember in MemberMatchesUser tests fully

--- src/Tests/Christofel.CtuAuth.Tests/Data/Ctu/Auth/GuildMemberRepository.cs
+++ src/Tests/Christofel.CtuAuth.Tests/Data/Ctu/Auth/GuildMemberRepository.cs
@@ -4,6 +4,7 @@
 //   Copyright (c) Christofel authors. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using Christofel.Common.Database.Models;
 using Remora.Discord.API.Abstractions.Objects;
@@ -17,19 +18,22 @@ namespace Christofel.CtuAuth.Tests.Data.Ctu.Auth
     /// </summary>
     public class GuildMemberRepository
     {
+        private static readonly Random _rng = new Random();
+
         /// <summary>
         /// Creates guild member using the specified <paramref name="user"/>.
         /// </summary>
         /// <param name="user">The user to be set.</param>
+        /// <param name="discordUsername">The username.</param>
         /// <returns>GuildMember representing the <paramref name="user"/>.</returns>
-        public static GuildMember CreateDummyGuildMember(DbUser user) => new
+        public static GuildMember CreateDummyGuildMember(DbUser user, string discordUsername = "DummyUser") => new
             (
-                new User(user.DiscordId, "DummyUser", 1234, default, default),
+                new User(user.DiscordId, discordUsername, 0, default, default),
                 default,
                 default,
                 default,
                 new List<Snowflake>(),
-                default,
+                DateTimeOffset.Now,
                 default,
                 default,
                 default,
