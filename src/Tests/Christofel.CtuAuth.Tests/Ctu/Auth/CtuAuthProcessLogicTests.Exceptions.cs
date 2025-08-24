2f4c3ef00a0d510c985d29233b0b3b9f58721c3a
Rutherther <rutherther@ditigal.xyz>
2025-08-24 13:14:48 +0200

    chore: fix typo

--- src/Tests/Christofel.CtuAuth.Tests/Ctu/Auth/CtuAuthProcessLogicTests.Exceptions.cs
+++ src/Tests/Christofel.CtuAuth.Tests/Ctu/Auth/CtuAuthProcessLogicTests.Exceptions.cs
@@ -1,5 +1,5 @@
 //
-//   CtuAtuhProcessLogicTests.Exceptions.cs
+//   CtuAuthProcessLogicTests.Exceptions.cs
 //
 //   Copyright (c) Christofel authors. All rights reserved.
 //   Licensed under the MIT license. See LICENSE file in the project root for full license information.
