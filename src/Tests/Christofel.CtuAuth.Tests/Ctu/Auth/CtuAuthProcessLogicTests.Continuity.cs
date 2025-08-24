3e9c82ca5998b77e5ca5461da06017e619ec3bea
Rutherther <rutherther@ditigal.xyz>
2025-08-24 14:33:51 +0200

    tests(ctuauth): more checks in CtuAuthProcess.Continuity tests
    - Add dummy successful conditions, steps, tasks
    - Always add more failing steps/tasks after the first failed one, verify the first one is the one returned
    - Add condition on all tasks being called on errors

--- src/Tests/Christofel.CtuAuth.Tests/Ctu/Auth/CtuAuthProcessLogicTests.Continuity.cs
+++ src/Tests/Christofel.CtuAuth.Tests/Ctu/Auth/CtuAuthProcessLogicTests.Continuity.cs
@@ -7,7 +7,6 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Christofel.CtuAuth;
 using Christofel.CtuAuth.Auth.Steps;
 using Christofel.CtuAuth.Auth.Tasks;
 using Christofel.CtuAuth.Extensions;
@@ -15,7 +14,7 @@ using Christofel.CtuAuth.Tests.Data.Ctu.Auth;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
-using Xunit;
+using Remora.Results;
 
 namespace Christofel.CtuAuth.Tests.Ctu.Auth
 {
@@ -71,14 +70,16 @@ namespace Christofel.CtuAuth.Tests.Ctu.Auth
         /// </summary>
         /// <returns>A <see cref="Task"/> that represents the asynchronous task.</returns>
         [Fact]
-        public async Task FailedConditionDoesntStartSteps()
+        public async Task FailedConditionDoesntStartStepsNorTasks()
         {
             var stepMock = new Mock<StepRepository.MockStep>();
+            var taskMock = new Mock<TaskRepository.MockTask>();
 
             IServiceProvider services = new ServiceCollection()
                 .AddCtuAuthProcess()
                 .AddAuthCondition<ConditionRepository.FailingCondition>()
-                .AddScoped<IAuthStep, StepRepository.MockStep>(p => stepMock.Object)
+                .AddTransient<IAuthStep>(_ => stepMock.Object)
+                .AddTransient<IAuthTask>(_ => taskMock.Object)
                 .AddLogging(b => b.ClearProviders())
                 .BuildServiceProvider();
 
@@ -102,6 +103,12 @@ namespace Christofel.CtuAuth.Tests.Ctu.Auth
             stepMock.Verify
             (
                 service => service.FillDataAsync(It.IsAny<IAuthData>(), It.IsAny<CancellationToken>()),
+                Times.Never
+            );
+
+            taskMock.Verify
+            (
+                service => service.ExecuteAsync(It.IsAny<IAuthData>(), It.IsAny<CancellationToken>()),
                 Times.Never
             );
         }
@@ -145,8 +152,68 @@ namespace Christofel.CtuAuth.Tests.Ctu.Auth
             );
         }
 
-        // TODO: failed task still completes other tasks
+        /// <summary>
+        /// Tests that if task fails, other tasks are still executed, even if exception is thrown.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous task.</returns>
+        [Fact]
+        public async Task ExecutesOtherTasksOnFailedTasks()
+        {
+            var taskMock1 = new Mock<TaskRepository.MockTask>();
+            var taskMock2 = new Mock<TaskRepository.MockTask>();
+            var taskMock3 = new Mock<TaskRepository.MockTask>();
+            var taskMock4 = new Mock<TaskRepository.MockTask>();
+            var taskMock5 = new Mock<TaskRepository.MockTask>();
 
-        // TODO: failed task returns soft auth error
+            Mock<TaskRepository.MockTask>[] mocks =
+                [taskMock1, taskMock2, taskMock3, taskMock4, taskMock5];
+
+            taskMock1.Setup(h => h.ExecuteAsync(It.IsAny<IAuthData>(), It.IsAny<CancellationToken>()))
+                .Throws<InvalidOperationException>();
+            taskMock2.Setup(h => h.ExecuteAsync(It.IsAny<IAuthData>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(Result.FromError(new DummyError("dummy2"))));
+            taskMock3.Setup(h => h.ExecuteAsync(It.IsAny<IAuthData>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(Result.FromError(new DummyError("dummy3"))));
+            taskMock4.Setup(h => h.ExecuteAsync(It.IsAny<IAuthData>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(Result.FromError(new DummyError("dummy4"))));
+            taskMock5.Setup(h => h.ExecuteAsync(It.IsAny<IAuthData>(), It.IsAny<CancellationToken>()))
+                .Throws<InvalidOperationException>();
+
+            IServiceProvider services = new ServiceCollection()
+                .AddCtuAuthProcess()
+                .AddCtuSuccessfulCST()
+                .AddTransient<IAuthTask>(p => taskMock1.Object)
+                .AddTransient<IAuthTask>(p => taskMock2.Object)
+                .AddTransient<IAuthTask>(p => taskMock3.Object)
+                .AddTransient<IAuthTask>(p => taskMock4.Object)
+                .AddTransient<IAuthTask>(p => taskMock5.Object)
+                .AddLogging(b => b.ClearProviders())
+                .BuildServiceProvider();
+
+            var user = await DbContext
+                .SetupUserToAuthenticateAsync();
+            var dummyGuildMember = CreateDummyGuildMember(user);
+            var successfulOauthHandler = GetMockedTokenApi(user);
+
+            var process = services.GetRequiredService<CtuAuthProcess>();
+            await process.FinishAuthAsync
+            (
+                DummyAccessToken,
+                successfulOauthHandler.Object,
+                DbContext,
+                DummyGuildId,
+                user,
+                dummyGuildMember
+            );
+
+            foreach (var mock in mocks)
+            {
+                mock.Verify
+                (
+                    service => service.ExecuteAsync(It.IsAny<IAuthData>(), It.IsAny<CancellationToken>()),
+                    Times.Once
+                );
+            }
+        }
     }
 }
